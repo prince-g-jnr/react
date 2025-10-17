b465f01fc5f2efb40278abd55baeea8dc90e2a60
Prince <gbolahanhardemuyiwa@gmail.com>
2025-10-17 10:01:16 +0100

    task

--- day_1/about_me_page/src/components/Footer.jsx
+++ day_1/about_me_page/src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
     return(
         <footer>
             <div className="social">
-                <a href="">Twitter</a>
-                <a href="">Linkedin</a>
-                <a href="">GitHub</a>
-                <a href="">Tiktok</a>
+                <a href="https://github.com/prince-g-jnr">GitHub</a>
+                <a href="https://x.com/prince_g_jnr">Twitter</a>
+                <a href="https://www.linkedin.com/in/gbolahan-ademuyiwa-45a559382">Linkedin</a>
+                <a href="https://www.tiktok.com/@prince_tk01">Tiktok</a>
             </div>
             <p>© {year} Prince. All Right Reserved.</p>
         </footer>
